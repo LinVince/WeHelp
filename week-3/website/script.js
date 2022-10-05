47dbc542a3641a15f6f9f4df6e93b18df1da7c9a
LinVince <38820721+LinVince@users.noreply.github.com>
2022-10-05 17:17:42 +0800

    Update script.js

--- week-3/website/script.js
+++ week-3/website/script.js
@@ -19,38 +19,75 @@ displaydata().then((data) => {
     
     
     const presentation1 = document.createElement('div');
-    presentation1.setAttribute('id','name');
     const presentation2 = document.createElement('div');
-    presentation2.setAttribute('id','name');
+    presentation_ = [presentation1,presentation2];
+    for (let i = 0; i < presentation_.length; i++){
+      presentation_[i].setAttribute('id','name');
+    }
+    //presentation1.setAttribute('id','name');
+    //presentation2.setAttribute('id','name');
     const preimg1 = document.createElement('img');
-    preimg1.setAttribute('src',photos[0]);
+    const preimg2 = document.createElement('img');
+    preimg_ = [preimg1,preimg2];
+    for (let i = 0; i < preimg_.length; i++){
+      preimg_[i].setAttribute('src',photos[i]);
+      preimg_[i].setAttribute('width','80px');
+      preimg_[i].setAttribute('height','50px');
+    };
+    /*preimg1.setAttribute('src',photos[0]);
     preimg1.setAttribute('width','80px');
     preimg1.setAttribute('height','50px');
-    const preimg2 = document.createElement('img');
+    
     preimg2.setAttribute('src',photos[1]);
     preimg2.setAttribute('width','80px');
-    preimg2.setAttribute('height','50px');
+    preimg2.setAttribute('height','50px');*/
     const pretitle1 = document.createElement('span');
-    pretitle1.innerText = stitles[0];
     const pretitle2 = document.createElement('span');
-    pretitle2.innerText = stitles[1];
     const galleryName = document.querySelector('.gallery-name');
-    galleryName.appendChild(presentation1);
+    presentation_ = [presentation1,presentation2];
+    pretitle_ = [pretitle1,pretitle2];
+    for (let i = 0; i < pretitle_.length; i++){
+      pretitle_[i].innerText = stitles[i]
+    };
+    /*pretitle1.innerText = stitles[0];
+    pretitle2.innerText = stitles[1];*/
+    for (let i = 0; i < pretitle_.length; i++){
+      galleryName.appendChild(presentation_[i]);
+      presentation_[i].appendChild(preimg_[i]);
+      presentation_[i].appendChild(pretitle_[i]);
+    };
+    /*galleryName.appendChild(presentation1);
     galleryName.appendChild(presentation2);
     presentation1.appendChild(preimg1);
     presentation2.appendChild(preimg2);
     presentation1.appendChild(pretitle1);
-    presentation2.appendChild(pretitle2);
+    presentation2.appendChild(pretitle2);*/
+    const showcase = document.querySelector('.showcase');
+    for (let i = 0; i < 4; i++){
+      var newf = document.createElement('div');
+      newf.setAttribute('class','photo');
+      showcase.appendChild(newf);
+      var img = document.createElement('img');
+      img.setAttribute('src',photos[i+2]);
+      img.setAttribute('width','290px');
+      img.setAttribute('height','200px');
+      newf.appendChild(img);
+      var newt = document.createElement('div');
+      newt.setAttribute('class','desc');
+      newt.innerText = stitles[i+2];
+      newf.appendChild(newt);
+      
+    };
+    /*f1 = document.createElement('div');
+    f2 = document.createElement('div');
+    f3 = document.createElement('div');
+    f4 = document.createElement('div');
+    f1.setAttribute('class','photo');    
+    f2.setAttribute('class','photo');    
+    f3.setAttribute('class','photo');    
+    f4.setAttribute('class','photo'); */
   
-    const f1 = document.createElement('div');
-    f1.setAttribute('class','photo');
-    const f2 = document.createElement('div');
-    f2.setAttribute('class','photo');
-    const f3 = document.createElement('div');
-    f3.setAttribute('class','photo');
-    const f4 = document.createElement('div');
-    f4.setAttribute('class','photo');
-    const t1 = document.createElement('span');
+    /*const t1 = document.createElement('span');
     t1.innerText = stitles[2];
     const t2 = document.createElement('span');
     t2.innerText = stitles[3];
@@ -74,11 +111,12 @@ displaydata().then((data) => {
     img4.setAttribute('src',photos[5]);
     img4.setAttribute('width','290px');
     img4.setAttribute('height','200px');
-    const showcase = document.querySelector('.showcase');
+    
     showcase.appendChild(f1);
     showcase.appendChild(f2);
     showcase.appendChild(f3);
     showcase.appendChild(f4);
+    
     f1.appendChild(img1);
     f2.appendChild(img2);
     f3.appendChild(img3);
@@ -87,9 +125,25 @@ displaydata().then((data) => {
     f1.appendChild(t1);
     f2.appendChild(t2);
     f3.appendChild(t3);
-    f4.appendChild(t4);
-    
-    const f5 = document.createElement('div');
+    f4.appendChild(t4);*/
+  
+    const showcase2 = document.querySelectorAll('.showcase')[1];
+    for (let i = 0; i < 4; i++){
+      var newf = document.createElement('div');
+      newf.setAttribute('class','photo');
+      showcase2.appendChild(newf);
+      var img = document.createElement('img');
+      img.setAttribute('src',photos[i+6]);
+      img.setAttribute('width','290px');
+      img.setAttribute('height','200px');
+      newf.appendChild(img);
+      var newt = document.createElement('div');
+      newt.setAttribute('class','desc');
+      newt.innerText = stitles[i+6];
+      newf.appendChild(newt);
+    };
+  
+    /*const f5 = document.createElement('div');
     f5.setAttribute('class','photo');
     const f6 = document.createElement('div');
     f6.setAttribute('class','photo');
@@ -134,7 +188,48 @@ displaydata().then((data) => {
     f5.appendChild(t5);
     f6.appendChild(t6);
     f7.appendChild(t7);
-    f8.appendChild(t8);
-   
-    
+    f8.appendChild(t8);*/
+    const showcase3 = document.querySelectorAll('.showcase')[2];
+    for (let i = 0; i < 4; i++){
+      var newf = document.createElement('div');
+      newf.setAttribute('class','photo');
+      showcase3.appendChild(newf);
+      var img = document.createElement('img');
+      img.setAttribute('src',photos[i+10]);
+      img.setAttribute('width','290px');
+      img.setAttribute('height','200px');
+      newf.appendChild(img);
+      var newt = document.createElement('div');
+      newt.setAttribute('class','desc');
+      newt.innerText = stitles[i+10];
+      newf.appendChild(newt);
+      showcase3.style.display = "none";
+    };
+  
+  
+    const showcase4 = document.querySelectorAll('.showcase')[3];
+    for (let i = 0; i < 4; i++){
+      var newf = document.createElement('div');
+      newf.setAttribute('class','photo');
+      showcase4.appendChild(newf);
+      var img = document.createElement('img');
+      img.setAttribute('src',photos[i+14]);
+      img.setAttribute('width','290px');
+      img.setAttribute('height','200px');
+      newf.appendChild(img);
+      var newt = document.createElement('div');
+      newt.setAttribute('class','desc');
+      newt.innerText = stitles[i+14];
+      newf.appendChild(newt);
+      showcase4.style.display = "none";
+    };      
 });
+function load(){
+      let showcase3 = document.querySelectorAll('.showcase')[2];
+      let showcase4 = document.querySelectorAll('.showcase')[3];
+      showcase3.style.display = "flex";
+      showcase4.style.display = "flex";
+      let btn = document.querySelector('button');
+      btn.style.display = "none";
+      }    
+
