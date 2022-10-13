a8b650e6f6b011d407297339162c6b53d4e53869
LinVince <vincejim91126@gmail.com>
2022-10-13 17:58:10 +0800

    better

--- week-4/app.py
+++ week-4/app.py
@@ -34,30 +34,49 @@ def signin():
     #POST = parameter is not embedded into the link
 
         #remove the user session and start over (if password if wrong)
+
         session.pop('user_id',None)
 
         #html name=""
         username = request.form['ac']
         password = request.form['pw']
 
+        if username == '' and password == '':
+            return render_template('signin.html')
+            
+
+        elif username == '' or password == '':
+            return redirect(url_for('error',message='請輸入帳號及密碼'))
+        
         #if the login account matches any of the accounts in the list
         #there will only one so [0]
-        try:
-            user = [x for x in users if x.username == username][0]
-            if user and user.password == password:
-                #let the server get the cookie so that the login status can remain
-                session['user_id'] = user.id
-                return redirect(url_for('member'))
-
-        except:
-            error = "You got a problem with your account or password."
-            return redirect(url_for('error',message="帳號密碼有誤"))
+        else:
+            try: 
+                user = [x for x in users if x.username == username][0]
+                if user.password == password:
+                    #let the server get the cookie so that the login status can remain
+                    session['user_id'] = user.id
+                    session['user_login'] = True
+                    return redirect(url_for('member'))
+                else:
+                    return redirect(url_for('error',message="帳號或密碼輸入錯誤"))
+            except:
+                return redirect(url_for('error',message="帳號或密碼輸入錯誤"))
 
     return render_template('signin.html')
 
 @app.route('/member')
 def member():
-    return render_template('member.html')
+    if session['user_login'] == True:
+        return render_template('member.html')
+    else:
+        return redirect(url_for('signin'))
+ 
+@app.route('/signout')
+def signout():
+    session['user_login'] = False
+    return redirect(url_for('signin'))
+
 
 @app.route('/error', methods = ['GET','POST'])
 def error():
